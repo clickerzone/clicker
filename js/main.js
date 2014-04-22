b9cd41d01fc0204547bfe9c8a61ff872ba15532d
Luis Gutierrez <luis.gt@outlook.com>
2014-04-22 10:08:23 -0600

    Start project

---
+++ js/main.js
@@ -0,0 +1,53 @@
+$(document).ready(function(){
+	var titles = $(".cycle-slide");
+	var logo = $("header h1");
+
+	$('nav').localScroll();
+
+	var s = skrollr.init({
+            edgeStrategy: 'set',
+            easing: {
+              WTF: Math.random,
+              inverted: function(p) {
+                return 1-p;
+              }
+            }
+          });
+
+	$('#hacemos').waypoint(function() {
+		if(logo.hasClass('invisible')){
+			$(logo).removeClass('invisible').addClass("fadeIn");
+		} else {
+			$(logo).addClass('invisible');
+		}
+		
+	});
+
+	if(titles.hasClass("cycle-slide-active")){
+		$(titles).each(function(index){
+			if(index===0){
+				$(this).addClass("bounceInDown");
+				}
+			if(index===1){
+				$(this).addClass("bounceInRight");
+				}
+			if(index===2){
+				$(this).addClass("bounceInUp");
+				}
+			if(index===3){
+				$(this).addClass("bounceInRight");
+				}
+			if(index===4){
+				$(this).addClass("bounceIn");
+				}
+			if(index===5){
+				$(this).addClass("bounceIn");
+				}
+		});
+	}
+
+
+	$('#navigation').localScroll();
+
+	
+});
